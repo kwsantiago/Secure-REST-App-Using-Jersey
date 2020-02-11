e38eef2fda2889b84152a5fd3822f18ede68859b
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-10 23:24:40 -0500

    Class that represents JSON config

---
+++ src/main/java/com/okta/jerseyrest/request/TaskRequest.java
@@ -0,0 +1,14 @@
+package com.okta.jerseyrest.request;
+
+public class TaskRequest {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
